8c8405131c25684b499e1ffa64bb2bc712844c8c
Dave Alaras <arroyo.daveil@gmail.com>
2020-04-02 18:03:12 +0800

    Fix  php://input and assign to REQUEST[‘’__INPUT]

--- libs/routes/slug_route.php
+++ libs/routes/slug_route.php
@@ -3,23 +3,25 @@ Configure::write('Api.MASTER_ROUTES','');
 class SlugRoute extends CakeRoute {
     function parse($url) {
 		$params = parent::parse($url);
-
-		$__INPUT =file_get_contents('php://input');
-
-		if($__INPUT):$_SESSION['__INPUT'] = $__INPUT;
-		elseif(isset($_SESSION['__INPUT'])):
-			$__INPUT = $_SESSION['__INPUT'];
-		endif;
-
-		$input = json_decode($__INPUT,true);
-
 		if (empty($params)) {
             return false;
         }
+
+		$__INPUT =file_get_contents('php://input');
+		
+		if($__INPUT):
+			$_REQUEST['__INPUT'] = $__INPUT;
+		elseif(isset($_REQUEST['__INPUT'])):
+			$__INPUT = $_REQUEST['__INPUT'];
+		endif;
+
+		$input = json_decode($__INPUT,true);
+		
+		
 		if(isset($input['id'])){
 			$params['pass']['id']=$input['id'];
 		}
-
+		
 		switch($_SERVER['REQUEST_METHOD']){
 			case 'DELETE':
 				$params['action']='delete';
@@ -27,11 +29,10 @@ class SlugRoute extends CakeRoute {
 			case 'PUT':
 				
 				if(is_array($input)):
-
 					foreach($input as $key=>$value)
 						$_POST[$key] = $value;
 				endif;
-				pr($_POST);
+				
 				$params['action']='edit';
 			break;
 		}
