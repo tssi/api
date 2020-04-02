5659b438a90ecd4a73ed9a3da272424ac6278aac
Dave Alaras <arroyo.daveil@gmail.com>
2020-04-02 17:51:47 +0800

    FIx Input session

--- libs/routes/slug_route.php
+++ libs/routes/slug_route.php
@@ -3,8 +3,15 @@ Configure::write('Api.MASTER_ROUTES','');
 class SlugRoute extends CakeRoute {
     function parse($url) {
 		$params = parent::parse($url);
-		
-		$input = json_decode(file_get_contents('php://input'),true);
+
+		$__INPUT =file_get_contents('php://input');
+
+		if($__INPUT):$_SESSION['__INPUT'] = $__INPUT;
+		elseif(isset($_SESSION['__INPUT'])):
+			$__INPUT = $_SESSION['__INPUT'];
+		endif;
+
+		$input = json_decode($__INPUT,true);
 
 		if (empty($params)) {
             return false;
@@ -12,15 +19,19 @@ class SlugRoute extends CakeRoute {
 		if(isset($input['id'])){
 			$params['pass']['id']=$input['id'];
 		}
+
 		switch($_SERVER['REQUEST_METHOD']){
 			case 'DELETE':
 				$params['action']='delete';
 			break;
 			case 'PUT':
+				
 				if(is_array($input)):
+
 					foreach($input as $key=>$value)
 						$_POST[$key] = $value;
 				endif;
+				pr($_POST);
 				$params['action']='edit';
 			break;
 		}
